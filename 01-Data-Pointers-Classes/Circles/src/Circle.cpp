f5c863080ab1935593376986bdaede85c522ce30
Ryan Bartley <ryanbartley@gmail.com>
2014-09-15 11:32:22 -0400

    Finished commenting Circle project

--- 01-Data-Pointers-Classes/Circles/src/Circle.cpp
+++ 01-Data-Pointers-Classes/Circles/src/Circle.cpp
@@ -6,46 +6,87 @@
 //
 //
 
-#include "Circle.h"
+// Here I'm including files from cinder, which will give me specific
+// functionality that I need for drawing and for random number generation.
 #include "cinder/gl/gl.h"
 #include "cinder/Rand.h"
+
+// I also need to include my Circle interface so that the compiler can
+// link the declared functionality (in my .h) with the defined functionality,
+// below.
+#include "Circle.h"
+
+// I include this file for the cout and endl functionality to log to the
+// screen.
 #include <iostream>
 
+// Using namespace tells the compiler that when looking for certain
+// functions to first check in whatever namespaces that i'm using.
+// For instance below, you'll see the objects 'cout' and 'endl'. Those
+// are within the 'std' namespace. When the compiler sees that it knows
+// that you're using the 'std' version of these objects. If you comment
+// out these lines, take a look at the errors you get and try to fix
+// them yourself.
 using namespace std;
 using namespace ci;
 
+// Here I am defining my declared default constructor. The syntax is very
+// similar to calling a function in a specific namespace. What I'm telling
+// the compiler, with Circle::Circle() is in the class Circle there is a function
+// called Circle(). Then I can define it.
 Circle::Circle()
-: mX( 0 ), mY( 0 ), mRadius( 10 ), mColor( randFloat(), randFloat(), randFloat() )
+: mX( 0 ), mY( 0 ), mRadius( 10 ),						// Here is my initializer list and this is almost
+	mColor( randFloat(), randFloat(), randFloat() )		// always going to be the way you initialize member vars
 {
+	// I'm using console out here, Note: without 'std::', to log when something uses
+	// this function. This is a great way to debug.
     cout << "I'm constructing with zero integers" << endl;
 }
 
+// Here I'm defining my Overloaded Constructor. There can be as many overloads
+// as you want but they have to differ in their arguments. I can't have (int, int)
+// and then make another (int, int) constructor but I could have (float, float)
+// the arguments must be different enough for the compiler to make a best match.
 Circle::Circle( int x, int y )
 : mX( x ), mY( y ), mRadius( 10 ), mColor( randFloat(), randFloat(), randFloat() )
 {
     cout << "I'm constructing with two integers" << endl;
 }
 
+// Here is the definition of Circle. Unlike constructors, destructors can't be
+// overloaded. There should be one way to destroy a specific class. We'll learn
+// next week though why this is and how to get around it.
 Circle::~Circle()
 {
     cout << "I'm destructing myself" << endl;
 }
 
+// Definition of getX which returns an int, specifically mX.
 int Circle::getX()
 {
     return mX;
 }
 
+// Definition of getY as above.
 int Circle::getY()
 {
     return mY;
 }
 
+// Definition of getRadius as above.
 int Circle::getRadius()
 {
     return mRadius;
 }
 
+// Definition of draw. What's great about this is that I can abstract away
+// how I want to draw my circle's and we'll see next week why this is powerful.
+// for now, imagine if I'm tweaking the way I'd like to present my circle, and
+// I have circle instances all over my program in different places drawing.
+// If I were to want to tweak my circle's drawing, I'd have to make changes
+// everywhere in my code. With the simple addition of a draw method, I can just
+// write mCircle.draw() in all of those places and come here to the source to
+// tweak knowing that all of those instances will share this functional implementation.
 void Circle::draw()
 {
     gl::color( mColor );
--- 01-Data-Pointers-Classes/Circles/src/CirclesApp.cpp
+++ 01-Data-Pointers-Classes/Circles/src/CirclesApp.cpp
@@ -3,12 +3,21 @@
 
 #include "Circle.h"
 
+// I include random functionality here. See Circle.cpp as well.
+// Even though I include it here, since I'm only including it
+// in Circle.cpp, this file can't see it because I haven't included
+// it. So I include files, everywhere that I'd like to see them.
 #include "cinder/Rand.h"
 
+// See Circle.cpp for an explanation of why we'd like to be using
+// namespaces.
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+// As we spoke about before, I use const and uppercase to illicit
+// the fact that if this number changes, it should be changed here
+// at the source.
 const int NUM_CIRCLES = 1000;
 
 class CirclesApp : public AppNative {
@@ -17,23 +26,62 @@ class CirclesApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
-    
+	
+	// This is a vector of shared_ptrs of type Circle. Using templates
+	// allows us to create complex meanings for types. We'll talk about
+	// this more next week.
     std::vector<std::shared_ptr<Circle>> mCircles;
-        
 };
 
 void CirclesApp::setup()
 {
-    
+    // I resize mCircles because I know how many I'd like to have. This
+	// makes it so that mCircles only goes to the operating system at most
+	// twice to allocate enough memory for the amount of circle shared_ptr's
+	// that I'm going to need. The use of a const here also makes it easy
+	// to read and easy to change if I want more. I just change it at
+	// the initialization point and it changes that number throughout the
+	// file.
     mCircles.resize( NUM_CIRCLES );
+	
+	// Vectors again act like arrays so I can iterate through the vector and
+	// initialize all of my Circle shared pointers.
     for( int i = 0; i < NUM_CIRCLES; ++i ) {
+		// reset is a member function of shared_ptr that takes as an argument a
+		// raw pointer and replaces whatever was stored in the shared_ptr before.
+		// in this case the shared_ptr's were containing a nullptr so it doesn't
+		// do anything other than store the raw Circle pointer we receive from
+		// the new operator. Look to mouseDown for further explanation.
         mCircles[i].reset( new Circle( randFloat( 0, getWindowWidth() ), randFloat( 0, getWindowHeight() ) ) );
+		
+		// This would be similar to the above. It just constructs a shared_ptr
+		// and passes it through the assignment operator
+		
+		// mCircles[i] = std::shared_ptr<Circle>( new Circle( randFloat( 0, getWindowWidth() ), randFloat( 0, getWindowHeight() ) ) );
     }
     
 }
 
 void CirclesApp::mouseDown( MouseEvent event )
 {
+	for( int i = 0; i < NUM_CIRCLES; ++i ) {
+		// This time we use reset it will destroy the past raw pointer using the
+		// delete operator. The reason is that when we constructed the shared_ptr's
+		// above, we only had one shared_ptr pointing to any one of them. Now,
+		// when reset recieves the raw pointer from new, it will check to see that
+		// it has a raw pointer already as a member variable, it will the decrement
+		// it's counter and find that it's counter is 0 at which point it will call
+		// delete on the raw pointer it already holds. Then it will take the raw
+		// pointer it was passed as an argument and store that address in it's member
+		// variable.
+		mCircles[i].reset( new Circle( randFloat( 0, getWindowWidth() ), randFloat( 0, getWindowHeight() ) ) );
+		
+		// This would be similar to the above. It just constructs a shared_ptr
+		// and passes it through the assignment operator doing the same check
+		// that reset will do.
+		
+		// mCircles[i] = std::shared_ptr<Circle>( new Circle( randFloat( 0, getWindowWidth() ), randFloat( 0, getWindowHeight() ) ) );
+	}
 }
 
 void CirclesApp::update()
@@ -44,8 +92,21 @@ void CirclesApp::draw()
 {
     // clear out the window with black
     gl::clear( Color( 0, 0, 0 ) );
+	
+	// Because we made the draw function inside of the circle class, drawing each
+	// circle becomes as easy as just calling draw. Whenever we want to change how
+	// we draw, we simply change the draw method inside the class and this will
+	// reflect our changes.
     for( int i = 0; i < NUM_CIRCLES; ++i ) {
+		// Instead of dot notation we use (->) pointer notation to get at the members
+		// of our instance.
         mCircles[i]->draw();
+		
+		// You can see what member functions shared_ptr has by uncommenting this line
+		// and putting a period (.) after the name and waiting for xcode to decide
+		// when it would like to start auto completeing :)
+		
+		// mCircles[i].
     }
     
     
